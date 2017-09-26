7cc25c53692d88af2d9600427edbbd2fd0b030e8
everysick <s.wakeup31@gmail.com>
2017-09-26 14:02:41 +0900

    Add log and norification

--- regexp_timeline.rb
+++ regexp_timeline.rb
@@ -2,12 +2,14 @@ require 'tweetstream'
 require 'twitter'
 
 puts "Loading twitter libraries."
+
 rest_client = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['CONSUMER_KEY']
   config.consumer_secret = ENV['CONSUMER_SECRET']
   config.access_token = ENV['REST_ACCESS_TOKEN']
   config.access_token_secret = ENV['REST_ACCESS_SECRET']
 end
+
 TweetStream.configure do |config|
   config.consumer_key = ENV['CONSUMER_KEY']
   config.consumer_secret = ENV['CONSUMER_SECRET']
@@ -15,25 +17,34 @@ TweetStream.configure do |config|
   config.oauth_token_secret = ENV['STR_ACCESS_SECRET']
   config.auth_method = :oauth
 end
+
 streaming_client = TweetStream::Client.new
 
 rest_user_id = ENV['REST_USER_ID']
 str_user_id = ENV['STR_USER_ID']
 
 puts "Loading regexp list."
+
 regexps = ENV['REGEXP'].split(',').map do |r|
   puts "/#{r.chomp}/"
   Regexp.new(r.chomp)
 end
 
 puts "Loading dis-allow regexp list."
+
 d_regexps = ENV['DIS_ALLOW_REGEXP'].split(',').map do |r|
   puts "/#{r.chomp}/"
   Regexp.new(r.chomp)
 end
 
 streaming_client.on_inited do
+  rest_client.create_direct_message(str_user_id, "start up")
   puts 'Connected!'
+end
+
+streaming_client.on_error do |message|
+  puts "Error: #{message}"
+  rest_client.create_direct_message(str_user_id, "faild down")
 end
 
 streaming_client.on_timeline_status do |status|
@@ -47,6 +58,8 @@ streaming_client.on_timeline_status do |status|
     puts "SendMessage: \n#{name}:#{id}\n#{text}\n#{url}"
     rest_client.create_direct_message(str_user_id, "#{name}:#{id}\n#{text}\n#{url}")
   end
+
+  puts "Message: #{status.text}"
 end
 
 streaming_client.userstream
